0b02539d92906e346d8d33949ed94dbf8dc2d580
Gunjan564 <145559194+Gunjan564@users.noreply.github.com>
2023-11-07 08:06:21 +0000

    commit first

---
+++ squarepattern.cpp
@@ -0,0 +1,31 @@
+//pattern printing
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i,r,x,n;
+    cout<<"GUNJAN"<<endl;
+    cout<<"Enter the number of squares you want to print: ";
+    cin>>n;
+    for(x=1;x<=n;x++)
+    {
+        for(i=1;i<5;i++)
+        {
+            for(r=1;r<5;r++)
+            {
+                if((i==2&&r==2)||(i==2&&r==3)||(i==3&&r==2)||(i==3&&r==3))
+                {
+                    cout<<"  ";
+                    continue;
+                }
+                cout<<"* ";
+            }
+            cout<<endl;    
+
+        }
+        cout<<endl;
+    }
+    
+    
+     
+}
