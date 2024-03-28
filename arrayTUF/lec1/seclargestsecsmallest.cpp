edc2d4151f5b0b1406928b10c216072aa5504d8c
pulkitsujaan <pulkitsujaan2708@gmail.com>
2024-03-28 20:06:13 +0530

    ipl

--- arrayTUF/lec1/seclargestsecsmallest.cpp
+++ arrayTUF/lec1/seclargestsecsmallest.cpp
@@ -3,8 +3,7 @@ using namespace std;
 int main()
 {   
     //Brute approach 
-    int i, j, b[2], a[] = {1, 2, 3, 4, 5}, n = 5;
-    cout << a[0] << endl;
+    int i, j, b[2], a[]={3,4,2,1,5}, n = 5;
     for (i = 0; i < n; i++)
     {
         for (j = 0; j < n; j++)
@@ -19,37 +18,65 @@ int main()
     }
     b[0] = a[1];
     b[1] = a[n - 2];
-    cout << b[0] << endl
-         << b[1];
+    cout <<"second smallest: "<< b[0] << endl
+         <<"second largest: "<< b[1];
     //Better approach
-    a[]={3,2,4,1,5};
-    int largest,seclargest,smallest;
+    int largest=a[0],seclargest=-1,smallest=a[0],secsmallest=largest+1;
     for(i=0;i<n;i++)
     {
-        if(a[i]>a[i+1]&&i!=n-1)
+        if(a[i]>largest)
         {
             largest=a[i];
         }
     }
+    
     for (int i = 0; i < n; i++)
     {
-        if (a[i]>a[i+1]&&i!=n-1&&a[i]<largest)
+        if (a[i]>seclargest&&a[i]<largest)
         {
             seclargest=a[i];
         }
     }
     for(i=0;i<n;i++)
     {
-        if(a[i]<a[i+1]&&i!=n-1)
+        if(a[i]<smallest)
         {
             smallest=a[i];
         }
     }
     for (int i = 0; i < n; i++)
     {
-        if (a[i]>a[i+1]&&i!=n-1&&a[i]<largest)
+        if (a[i]<secsmallest&&a[i]>smallest)
+        {
+            secsmallest=a[i];
+        }
+    }
+    cout<<endl<<seclargest<<endl<<secsmallest;
+    //optimal approach
+    seclargest=-2;largest=-1;secsmallest=INT32_MAX;smallest=INT32_MAX-1;
+    for(i=0;i<n;i++)
+    {
+        if(a[i]>largest)
+        {
+            seclargest=largest;
+            largest=a[i];
+        }
+        else if(a[i]>seclargest&&a[i]!=largest)
         {
             seclargest=a[i];
         }
     }
-}
+    for(i=0;i<n;i++)
+    {
+        if(a[i]<smallest)
+        {
+            secsmallest=smallest;
+            smallest=a[i];
+        }
+        else if(a[i]<secsmallest&&a[i]!=smallest)
+        {
+            secsmallest=a[i];
+        }
+    }
+    cout<<endl<<"seclargest: "<<seclargest<<endl<<"secsmallest: "<<secsmallest;
+} 
