7a33ee716b4b288ae5ad8ca618b7e3e55df5068a
Gunjan564 <gunjan231103@gmail.com>
2024-04-30 13:53:09 +0530

    vinay

--- arrayTUF/lec4/arraysumk.cpp
+++ arrayTUF/lec4/arraysumk.cpp
@@ -2,15 +2,25 @@
 using namespace std;
 int main()
 {
-   int a[]={1,2,3,1,1,1,4,2,3},i,j;
+   int a[]={1,2,3,1,1,1,4,2,3},i,j,from=-1,to=-5,k=3,sum=0,size=-1;
    //brute approach
    for(i=0;i<sizeof(a)/4;i++)
    {
       for(j=0;j<sizeof(a)/4;j++)
       {
-         if(sum>k)
-         sum=sum+a[i];
+         sum=sum+a[j];
+         if(sum==k&&size<j-i){
+            from=i;
+            to=j;
+            size=to-from;
+            break;
+         }
+         if(sum>k){
+            break;
+         }
+         
       }
    }
+   cout<<from<<endl<<to;
    return 0;
 }
