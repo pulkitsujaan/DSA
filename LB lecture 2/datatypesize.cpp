dc75eef6bf5f2dc98cccffd183fc54004ef88a42
Gunjan564 <gunjan231103@gmail.com>
2024-06-07 22:21:47 +0530

    fgnhg

--- LB lecture 2/datatypesize.cpp
+++ LB lecture 2/datatypesize.cpp
@@ -20,8 +20,8 @@ int main()
     cout<<"size of int = "<<sizeof(int)<<"\n";
     cout<<"size of float = "<<sizeof(float)<<"\n";
     cout<<"size of char = "<<sizeof(char)<<"\n";
-    cout<<"size of char = "<<sizeof(double)<<"\n";
-    cout<<"size of char = "<<sizeof(long)<<"\n";
+    cout<<"size of double = "<<sizeof(double)<<"\n";
+    cout<<"size of long = "<<sizeof(long)<<"\n";
     
     bool check = (f==d);
     cout<<"check = "<<check<<"\n";
