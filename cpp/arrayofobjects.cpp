05a6d4fc6897b96351baff276fd6b6485ee3b41f
Gunjan564 <gunjan231103@gmail.com>
2024-06-17 22:53:31 +0530

    imissyou

--- cpp/arrayofobjects.cpp
+++ cpp/arrayofobjects.cpp
@@ -1,7 +1,31 @@
 #include <iostream>
 using namespace std;
-int main()
-{
-   
-   return 0;
-}
+
+   class data
+   {
+   private:
+      int id;
+      string colour;
+   public:
+      void SetData(void){
+         cout<<"Enter the student's id: ";
+         cin>>id;
+         cout<<"Enter your colour: ";
+         cin>>colour;
+      };
+      void DisplayData(void){
+         cout<<"Student's id: "<<id<<endl;
+         cout<<"Student's colour: "<<colour<<endl;
+      };
+   };   
+   int main()
+   {
+      data students[4];
+      for(int i=0;i<3;i++){
+         students[i].SetData();
+      }
+      for(int i=0;i<3;i++){
+         students[i].DisplayData();         
+      }   
+      return 0;
+   }
